2dbd363ce1e02ef51a19035aa787c98b741fe86c
favtuts <favtuts@gmail.com>
2022-08-28 17:37:01 +0700

    Filtering APIs for Swagger

--- sources/spring-boot-starter-app/src/main/java/com/favtuts/demo/sbstarter/configurations/SwaggerConfig.java
+++ sources/spring-boot-starter-app/src/main/java/com/favtuts/demo/sbstarter/configurations/SwaggerConfig.java
@@ -11,11 +11,23 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-    @Bean
+    /*@Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }*/
+
+    // Filtering APIs for Swagger
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.favtuts.demo.sbstarter.controllers"))
+                //.paths(PathSelectors.ant("/api/*"))
+                //.paths(PathSelectors.ant("/api/vehicles/*"))
                 .paths(PathSelectors.any())
                 .build();
     }
