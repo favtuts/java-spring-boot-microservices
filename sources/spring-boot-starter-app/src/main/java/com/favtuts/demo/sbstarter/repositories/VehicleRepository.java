26184fc49d11739b8b26c2d241592909d36f7e55
favtuts <favtuts@gmail.com>
2022-08-28 11:43:08 +0700

    Selecting records based on condition

--- sources/spring-boot-starter-app/src/main/java/com/favtuts/demo/sbstarter/repositories/VehicleRepository.java
+++ sources/spring-boot-starter-app/src/main/java/com/favtuts/demo/sbstarter/repositories/VehicleRepository.java
@@ -2,10 +2,14 @@ package com.favtuts.demo.sbstarter.repositories;
 
 import com.favtuts.demo.sbstarter.entities.Vehicle;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
 import java.util.UUID;
 
 @RepositoryRestResource(collectionResourceRel = "cars", path = "cars")
 public interface VehicleRepository extends CrudRepository<Vehicle, UUID> {
+
+    List<Vehicle> findByMake(@Param("make") String make);
 }
