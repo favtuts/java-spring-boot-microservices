18220945d9cbf5169622e69b860e55164bba6748
favtuts <favtuts@gmail.com>
2022-08-28 20:12:44 +0700

    Enabling AuditorAware interface and audit functionality

---
+++ sources/spring-boot-starter-app/src/main/java/com/favtuts/demo/sbstarter/configurations/PersistenceConfig.java
@@ -0,0 +1,17 @@
+package com.favtuts.demo.sbstarter.configurations;
+
+import com.favtuts.demo.sbstarter.entities.AuditorAwareImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
+public class PersistenceConfig {
+
+    @Bean
+    public AuditorAware<String> auditorAware() {
+        return new AuditorAwareImpl();
+    }
+}
