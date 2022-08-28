2c1904d8281147d493589ea5051851c131654976
favtuts <favtuts@gmail.com>
2022-08-28 11:17:56 +0700

    Customizing the REST End-point

--- sources/spring-boot-starter-app/src/main/java/com/favtuts/demo/sbstarter/repositories/VehicleRepository.java
+++ sources/spring-boot-starter-app/src/main/java/com/favtuts/demo/sbstarter/repositories/VehicleRepository.java
@@ -2,8 +2,10 @@ package com.favtuts.demo.sbstarter.repositories;
 
 import com.favtuts.demo.sbstarter.entities.Vehicle;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.UUID;
 
+@RepositoryRestResource(collectionResourceRel = "cars", path = "cars")
 public interface VehicleRepository extends CrudRepository<Vehicle, UUID> {
 }
