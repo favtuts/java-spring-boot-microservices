2fc2594fd4dc81cbd01a6b78a2086d7a5884113a
favtuts <favtuts@gmail.com>
2022-08-28 18:15:37 +0700

    override response messages of HTTP methods globally

--- sources/spring-boot-starter-app/src/main/java/com/favtuts/demo/sbstarter/configurations/SwaggerConfig.java
+++ sources/spring-boot-starter-app/src/main/java/com/favtuts/demo/sbstarter/configurations/SwaggerConfig.java
@@ -2,15 +2,21 @@ package com.favtuts.demo.sbstarter.configurations;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMethod;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -22,7 +28,10 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.basePackage("com.favtuts.demo.sbstarter.controllers"))
                 .paths(PathSelectors.any())
                 .build()
-                .apiInfo(getApiInformation());
+                .apiInfo(getApiInformation())
+                .useDefaultResponseMessages(false)
+                .globalResponseMessage(RequestMethod.GET, getCustomizedResponseMessages()
+                );
     }
 
     private ApiInfo getApiInformation(){
@@ -35,5 +44,12 @@ public class SwaggerConfig {
                 "API License URL",
                 Collections.emptyList()
         );
+    }
+
+    private List<ResponseMessage> getCustomizedResponseMessages(){
+        List<ResponseMessage> responseMessages = new ArrayList<>();
+        responseMessages.add(new ResponseMessageBuilder().code(500).message("Server has crashed!!").responseModel(new ModelRef("Error")).build());
+        responseMessages.add(new ResponseMessageBuilder().code(403).message("You shall not pass!!").build());
+        return responseMessages;
     }
 }
