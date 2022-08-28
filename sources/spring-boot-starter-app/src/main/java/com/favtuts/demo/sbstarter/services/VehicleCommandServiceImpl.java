2344422f6c408798ee3fa11e92548cb84cd3e0b8
favtuts <favtuts@gmail.com>
2022-08-28 15:34:08 +0700

    apply Exception Handling for EntityNotFoundException

--- sources/spring-boot-starter-app/src/main/java/com/favtuts/demo/sbstarter/services/VehicleCommandServiceImpl.java
+++ sources/spring-boot-starter-app/src/main/java/com/favtuts/demo/sbstarter/services/VehicleCommandServiceImpl.java
@@ -9,6 +9,7 @@ import com.favtuts.demo.sbstarter.repositories.VehicleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.UUID;
 
 @Service
@@ -43,7 +44,7 @@ public class VehicleCommandServiceImpl implements VehicleCommandService {
             return new VehicleQueryDTO(updatedVehicle.getId(), updatedVehicle.getVehicleIdentityNumber(),
                     updatedVehicle.getMake(), updatedVehicle.getModel());
         } else {
-            return null;
+            throw new EntityNotFoundException("Vehicle Id " + id.toString() + " not found in the database");
         }
     }
 }
